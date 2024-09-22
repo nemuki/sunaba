16e988b70f81c884cb66d2333226924f3a144a1c
Naoki Takahashi <43571743+nemuki@users.noreply.github.com>
2024-09-23 08:54:12 +0900

    コネクションからクエリまで追加

--- sqlc-kotlin/src/main/kotlin/Main.kt
+++ sqlc-kotlin/src/main/kotlin/Main.kt
@@ -1,5 +1,34 @@
 package dev.nemuki
 
+import dev.nemuki.infra.repository.mapper.sandbox.QueriesImpl
+import java.sql.DriverManager
+
 fun main() {
     println("Hello World!")
+
+    val connection = DriverManager.getConnection(
+        "jdbc:postgresql://localhost:5432/sunaba",
+        "sunaba",
+        "password",
+    )
+
+    try {
+        val queries = QueriesImpl(connection)
+        val data = queries.listSandboxData()
+
+        println("Sandbox data:")
+        data.forEach {
+            println(it)
+        }
+
+        val dataById = queries.getSandboxDataById(1)
+
+        println("Sandbox data by id:")
+        println(dataById)
+    } catch (e: Exception) {
+        println("Failed to set autocommit to false")
+        e.printStackTrace()
+    } finally {
+        connection.close()
+    }
 }
