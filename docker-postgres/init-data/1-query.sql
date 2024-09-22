16aa9aa66c425d7956453dd7085331ea4ae56ed5
Naoki Takahashi <43571743+nemuki@users.noreply.github.com>
2024-09-22 10:58:39 +0900

    テストデータ追加

---
+++ docker-postgres/init-data/1-query.sql
@@ -0,0 +1,6 @@
+INSERT INTO main.sandbox_data (username, email, created_at) VALUES
+('user1', 'user1@example.com', CURRENT_TIMESTAMP),
+('user2', 'user2@example.com', CURRENT_TIMESTAMP),
+('user3', 'user3@example.com', CURRENT_TIMESTAMP),
+('user4', 'user4@example.com', CURRENT_TIMESTAMP),
+('user5', 'user5@example.com', CURRENT_TIMESTAMP);
