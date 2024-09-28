bd30caa2d724500de64639b0a6e3e4bc60c0ebed
Naoki Takahashi <43571743+nemuki@users.noreply.github.com>
2024-09-29 07:47:39 +0900

    json placeholder で API コール https://qiita.com/gitcho/items/71b2b2f66356c6de2502

--- kotlin-call-web-api/src/main/kotlin/Main.kt
+++ kotlin-call-web-api/src/main/kotlin/Main.kt
@@ -1,5 +1,22 @@
-package dev.nemuki
+import java.net.URI
+import java.net.http.HttpClient
+import java.net.http.HttpRequest
+import java.net.http.HttpResponse
 
 fun main() {
     println("Hello World!")
+
+    val client = HttpClient.newHttpClient()
+    val request = HttpRequest
+        .newBuilder(
+            URI.create("https://jsonplaceholder.typicode.com/posts"),
+        )
+        .build()
+
+    val response = client.send(
+        request,
+        HttpResponse.BodyHandlers.ofString()
+    )
+
+    println(response.body())
 }
